04aa87eb95e2c63e5172b5c88acaebbdf803171a
SKulLHelL <90751762+SKulLHelL@users.noreply.github.com>
2023-05-21 18:06:00 +0300

    Create main.py

---
+++ Lab 2/main.py
@@ -0,0 +1,2 @@
+data = open(r"T:\.temp\123.csv",'r',encoding="utf-8").read()
+print("DNS имен из трафика: "+str(data.count('CNAME'))+"; % нежелательного трафика - "+str(round(data.count('CNAME')*100/data.count(',"DNS",'),3)))
